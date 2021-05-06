ad9ffae471ccda1e7a0833f3bca1c4b50b8c0b1e
Claire Devlin <claire.devlin@hotmail.com>
2021-05-06 17:02:46 -0400

    added tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,17 @@
 const request = require('request');
-const args = process.argv.slice(2);
 
-request('https://api.thecatapi.com/v1/breeds/search?q=' + args[0], (error, response, body) => {
-  if (error) {
-    console.log(error);
-    throw error;
-  }
-  if (body === []) {
-    console.log("Breed not found");
-    return;
-  }
-  const description = JSON.parse(body)[0].description;
-  console.log(description);
+const fetchBreedDescription = function (breedName, callback) {
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName,
+    ///what do do with response from website
+    function (error, response, body) {
+      if (error) {
+        callback(error, null);
+      } else if (body === '[]') {
+        callback("Breed not found", null);
+      } else {
+        callback(null, JSON.parse(body)[0].description);
+      }
+    });
+};
 
-});
+module.exports = { fetchBreedDescription };
---
+++ index.js
@@ -0,0 +1,11 @@
+const breedName = process.argv[2];
+const { fetchBreedDescription } = require('./breedFetcher');
+
+//(args[0], )
+fetchBreedDescription(breedName, (error, desc) => {
+  if (error) {
+    console.log('Error fetch details: ', error);
+  } else {
+    console.log(desc);
+  }
+});
